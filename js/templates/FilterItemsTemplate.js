3a91c96d8a27050c93a3b4d24589be76f2b238e7
BARRETCHRIS <christelle.barret.poirot@gmail.com>
2024-03-06 09:52:39 +0100

    Adjust array for clics on items filters

--- js/templates/FilterItemsTemplate.js
+++ js/templates/FilterItemsTemplate.js
@@ -2,7 +2,9 @@ import ListItemsClickHandler from '../utils/ListItemsClickHandler.js';
 
 export default class FilterItemsTemplate {
     constructor() {
-        this.listItemClickHandler = new ListItemsClickHandler();
+        // Utilise une seule instance de ListItemsClickHandler pour toutes les listes
+        this.listItemClickHandler = FilterItemsTemplate.listItemClickHandler || new ListItemsClickHandler();
+        FilterItemsTemplate.listItemClickHandler = this.listItemClickHandler;
     }
 
     createListItem(item, listType) {
@@ -20,3 +22,26 @@ export default class FilterItemsTemplate {
         this.listItemClickHandler.handleClick(item, listType);
     }
 }
+
+// import ListItemsClickHandler from '../utils/ListItemsClickHandler.js';
+
+// export default class FilterItemsTemplate {
+//     constructor() {
+//         this.listItemClickHandler = new ListItemsClickHandler();
+//     }
+
+//     createListItem(item, listType) {
+//         const listItem = document.createElement('li');
+//         listItem.textContent = item;
+//         listItem.classList.add(`${listType}Item`);
+
+//         // Ajoute un écouteur de clic pour réagir aux clics sur les balises <li>
+//         listItem.addEventListener('click', () => this.handleListItemClick(item, listType));
+
+//         return listItem;
+//     }
+
+//     handleListItemClick(item, listType) {
+//         this.listItemClickHandler.handleClick(item, listType);
+//     }
+// }
--- js/utils/ListItemsClickHandler.js
+++ js/utils/ListItemsClickHandler.js
@@ -1,4 +1,4 @@
-class ListItemsClickHandler {
+export default class ListItemsClickHandler {
     constructor() {
         this.selectedItems = [];
     }
@@ -33,6 +33,7 @@ class ListItemsClickHandler {
                     element.classList.add('selected');
                     element.setAttribute('aria-checked', 'true');
                 } else {
+                    // On ne retire pas la classe "selected" ici pour éviter la désélection
                     element.setAttribute('aria-checked', 'false');
                 }
             }
@@ -42,8 +43,58 @@ class ListItemsClickHandler {
     // Méthode appelée pour manipuler les éléments sélectionnés
     handleSelectedItems() {
         console.log('Selected Items:', this.selectedItems);
-        // Ajoute ici votre logique pour traiter les éléments sélectionnés
+        // Ajoutez ici votre logique pour traiter les éléments sélectionnés
+    }
+
+    getSelectedItems() {
+        return this.selectedItems;
     }
 }
 
-export default ListItemsClickHandler;
+// export default class ListItemsClickHandler {
+//     constructor() {
+//         this.selectedItems = [];
+//     }
+
+//     handleClick(item, listType) {
+//         // Vérifie si l'élément est déjà sélectionné
+//         const isSelected = this.isSelected(item, listType);
+
+//         if (!isSelected) {
+//             // L'élément n'est pas encore sélectionné, donc le sélectionner
+//             this.selectedItems.push({ text: item, listType });
+
+//             // Sélectionne l'élément visuellement
+//             this.updateItemVisualState(item, listType, true);
+//         }
+
+//         this.handleSelectedItems();
+//     }
+
+//     // Méthode pour vérifier si un élément est déjà sélectionné
+//     isSelected(item, listType) {
+//         return this.selectedItems.some(selectedItem => selectedItem.listType === listType && selectedItem.text === item);
+//     }
+
+//     // Méthode pour mettre à jour l'état visuel de l'élément
+//     updateItemVisualState(item, listType, isSelected) {
+//         const itemElements = document.querySelectorAll(`.${listType}Item`);
+
+//         itemElements.forEach(element => {
+//             if (element.textContent === item) {
+//                 if (isSelected) {
+//                     element.classList.add('selected');
+//                     element.setAttribute('aria-checked', 'true');
+//                 } else {
+//                     element.setAttribute('aria-checked', 'false');
+//                 }
+//             }
+//         });
+//     }
+
+//     // Méthode appelée pour manipuler les éléments sélectionnés
+//     handleSelectedItems() {
+//         console.log('Selected Items:', this.selectedItems);
+//         // Ajoute ici votre logique pour traiter les éléments sélectionnés
+//     }
+// }
